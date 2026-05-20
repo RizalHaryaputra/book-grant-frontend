12001c5c9a93b477c53b68fd2631dd0f8acac3b5
MuhammadDamarZakyAlAyyubi <damarzaky123@gmail.com>
2026-05-21 00:08:14 +0700

    fix frontend routing and pages

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-<script setup>
-import DashboardPenulis from './views/DashboardPenulis.vue'
-</script>
-
 <template>
   <router-view />
 </template>
-=======
-<script setup></script>
-
-<template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
-
-<style scoped></style>
->>>>>>> e30f51b4e5c1b6c8f777afb9b48fe6e57b7c644c
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-<<<<<<< HEAD
 import router from './router'
+
+import './style.css'
 
 createApp(App)
   .use(router)
   .mount('#app')
-=======
-createApp(App).mount('#app')
->>>>>>> e30f51b4e5c1b6c8f777afb9b48fe6e57b7c644c
